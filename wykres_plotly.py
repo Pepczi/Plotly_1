c65fa34f3c3949553a8199f77f3e5b26238f8e93
Piotr Pawełczak <pawelczak.piotr@gmail.com>
2021-03-18 17:23:18 +0100

    zadanie_1

---
+++ wykres_plotly.py
@@ -0,0 +1,18 @@
+import plotly.graph_objects as go 
+
+salaries = [
+	("Mark", 1000),
+	("John", 1500),
+	("Daniel", 2300),
+	("Greg", 5000)
+	]
+
+names = list(map(lambda tup:tup[0], salaries))
+salary_values = list(map(lambda tup:tup[1], salaries))
+
+data = go.Bar(x=names, y=salary_values)
+layout = {
+    'title' : 'Pensje pracowników'
+    }
+fig = go.Figure(data, layout)
+fig.show()
